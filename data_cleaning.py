70e646e75b2c362dc3951f4d5e2cbe67255037b6
Jayapriya99 <jayapriya_varadharajan@hotmail.com>
2022-06-20 23:22:46 +0800

    Deleted Speed of Sound Columns

--- data_cleaning.py
+++ data_cleaning.py
@@ -12,4 +12,20 @@ df = pd.concat(
     map(pd.read_csv, joined_list), ignore_index=True)
 print(df)
 
+#Deleting extra columns
+data = pd.read_csv("Meters_mod.csv")
+
+data.drop('Speed of Sound1', inplace=True, axis=1)
+data.drop('Speed of Sound2', inplace=True, axis=1)
+data.drop('Speed of Sound3', inplace=True, axis=1)
+data.drop('Speed of Sound4', inplace=True, axis=1)
+data.drop('Speed of Sound5', inplace=True, axis=1)
+data.drop('Speed of Sound6', inplace=True, axis=1)
+data.drop('Speed of Sound7', inplace=True, axis=1)
+data.drop('Speed of Sound8', inplace=True, axis=1)
+
+print(data)
+
+
+
 df.to_csv("Meters_mod.csv", index=False)
